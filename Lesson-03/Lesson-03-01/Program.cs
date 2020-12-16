648ef2c8dbef8fa90a9f7cac6bad8df11e9c9f1f
Verm-V <Verm-V@yandex.ru>
2020-12-16 14:49:56 +0600

    переделал заполнение массива случайными числами с использованием GetLength

--- Lesson-03/Lesson-03-01/Program.cs
+++ Lesson-03/Lesson-03-01/Program.cs
@@ -21,8 +21,13 @@ namespace Lesson_03_04
 
             //заполняем массив рандомной чепухой
             Random rnd = new Random();
-            for (int i = 0; i < array.Length; i++)
-                array[i / columns, i % columns] = rnd.Next(100);
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(100);
+                }
+            }
 
             //вывод всего массива на экран
             //ради интереса сделал без использования вложенных циклов

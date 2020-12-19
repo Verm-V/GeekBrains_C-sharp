0f660c5c51b6c52219bdb9575f0cad1a78178ac8
Verm-V <Verm-V@yandex.ru>
2020-12-20 04:04:20 +0600

    1-е и 2-е задание 4-го урока выполнено

--- Lesson-04/Lesson-04-01/Program.cs
+++ Lesson-04/Lesson-04-01/Program.cs
@@ -8,8 +8,54 @@ namespace Lesson_04_01
 {
     class Program
     {
+        /// <summary> количество запросов ФИО </summary>
+        const int MAX = 5;
+
         static void Main(string[] args)
         {
+            string lastName;
+            string firstName;
+            string patronimic;
+            string fullName;
+
+            for (int i = 0; i < MAX; i++)
+            {
+                lastName = StringInput("Введите фамилию: ");
+                firstName = StringInput("Введите имя: ");
+                patronimic = StringInput("Введите отчество: ");
+                fullName = GetFullName(lastName, firstName, patronimic);
+                Console.WriteLine($"ФИО: {fullName}\n");
+            }
+
+            Console.WriteLine("--------\npress any key\n");
+            Console.ReadKey();
+
         }
+
+        /// <summary>
+        /// Запрашивает у пользователя ввод строки с заданным сообщением
+        /// </summary>
+        /// <param name="message">собщение для пользователя</param>
+        /// <returns>строка введенная пользователем</returns>
+        private static string StringInput(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Объединяет вместе три строки: имя, фамилию и отчество
+        /// </summary>
+        /// <param name="lastName">фамилия</param>
+        /// <param name="firstName">имя</param>
+        /// <param name="patronymic">отчество</param>
+        /// <returns>полное Фамилию Имя Отчество</returns>
+        private static string GetFullName(string lastName, string firstName, string patronymic)
+        {
+            string fullName = lastName + " " + firstName + " " + patronymic; ;
+            return fullName;
+        }
+
+
     }
 }
--- Lesson-04/Lesson-04-02/Program.cs
+++ Lesson-04/Lesson-04-02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,59 @@ namespace Lesson_04_02
 {
     class Program
     {
+        /// <summary> разделитель между числами в вводимой строке </summary>
+        const char separator = ' '; 
         static void Main(string[] args)
         {
+            bool isTextGood = false;//флаг проверки введеной строки на соответсвие условиям задачи
+            float sum = 0;//здесь будет сумма строк из чисел
+
+            while (!isTextGood)//цикл будет крутится пока строка не пройдет все проверки
+            {
+                //запрашиваем у пользователя строку чисел разделенных пробелами
+                string text = StringInput("Введите список чисел отделенных пробелами друг от друга:\n");
+
+                //разбиваем ее на массив строк, с удалением пустых строк (если будет несколько пробелов подряд)
+                string[] textNumbers = text.Split(new char[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+
+                float number = 0;
+
+                sum = 0;
+                isTextGood = true;
+
+                //пытаемся конвертировать каждую строку в массиве в число и прибавить его к сумме
+                //если какое-то из элементов не конвертируется в массив, то сбрасываем флаг проверки
+                //и цикл перезапустится с новым запросом строки у пользователя
+                foreach (string textNumber in textNumbers)
+                {
+                    if (!float.TryParse(textNumber, out number))
+                    {
+                        isTextGood = false;
+                        if (!isTextGood) Console.WriteLine("введенная строка не соответствует критериям, повторите ввод.\n");
+                        number = 0;
+                    }
+                    sum += number;
+                }
+                
+            }
+
+
+            Console.WriteLine($"\nСумма всех чисел в введенной строке равна {sum}");
+
+            Console.WriteLine("\n--------\npress any key\n");
+            Console.ReadKey();
         }
+
+        /// <summary>
+        /// Запрашивает у пользователя ввод строки с заданным сообщением
+        /// </summary>
+        /// <param name="message">собщение для пользователя</param>
+        /// <returns>строка введенная пользователем</returns>
+        private static string StringInput(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
     }
 }

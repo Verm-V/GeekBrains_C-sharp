47916e5e54b032dde549530de09f9c0d872c6dad
Verm-V <Verm-V@yandex.ru>
2020-12-22 17:25:42 +0600

    1-е задание 5-го урока выполнео

--- Lesson-05/Lesson-05-01/Program.cs
+++ Lesson-05/Lesson-05-01/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_05_01
+namespace Lesson_05_05
 {
     class Program
     {
+        const string filename = "text.txt";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Пожалуйста, введите любой набор символов:");
+            string text = Console.ReadLine();
+
+            File.WriteAllText(filename, text);
+
+            Console.WriteLine("\n--------\npress any key\n");
+            Console.ReadKey();
+
         }
+
     }
 }
